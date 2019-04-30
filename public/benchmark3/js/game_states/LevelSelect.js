c430a31d678454b50e294694eb4d5a1a154f32d3
Kristian Charbonneau <kristiancharbonneau@Kristians-MacBook-Pro.local>
2019-04-30 13:17:39 -0400

    commented out line to make level select buttons grey

--- public/benchmark3/js/game_states/LevelSelect.js
+++ public/benchmark3/js/game_states/LevelSelect.js
@@ -30,7 +30,7 @@ GeometrySplit.LevelSelect.prototype = {
 
       buttonStart2 = GeometrySplit.game.add.text(30, 300+50, 'Stage 2', {});
       buttonStart2.inputEnabled = true;
-      buttonStart2.addColor('#d3d3d3', 0);
+      //buttonStart2.addColor('#d3d3d3', 0);
       buttonStart2.events.onInputDown.add((e) => {
         GeometrySplit.levelNum = 2;
         GeometrySplit.game.state.start('Game');
@@ -38,7 +38,7 @@ GeometrySplit.LevelSelect.prototype = {
 
       buttonStart3 = GeometrySplit.game.add.text(30, 300+100, 'Stage 3', {});
       buttonStart3.inputEnabled = true;
-      buttonStart3.addColor('#d3d3d3', 0);
+      //buttonStart3.addColor('#d3d3d3', 0);
       buttonStart3.events.onInputDown.add((e) => {
         GeometrySplit.levelNum = 3;
         GeometrySplit.game.state.start('Game');
@@ -46,14 +46,14 @@ GeometrySplit.LevelSelect.prototype = {
 
       buttonStart4 = GeometrySplit.game.add.text(30, 300+150, 'Stage 4', {});
       buttonStart4.inputEnabled = true;
-      buttonStart4.addColor('#d3d3d3', 0);
+      //buttonStart4.addColor('#d3d3d3', 0);
       buttonStart4.events.onInputDown.add((e) => {
         GeometrySplit.levelNum = 4;
         GeometrySplit.game.state.start('Game');
       }, this);
       buttonStart5 = GeometrySplit.game.add.text(30, 300+200, 'Stage 5', {});
       buttonStart5.inputEnabled = true;
-      buttonStart5.addColor('#d3d3d3', 0);
+      //buttonStart5.addColor('#d3d3d3', 0);
       buttonStart5.events.onInputDown.add((e) => {
         GeometrySplit.levelNum = 5;
         GeometrySplit.game.state.start('Game');
